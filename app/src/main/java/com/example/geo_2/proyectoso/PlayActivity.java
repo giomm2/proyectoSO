9777e51c5d152214adb20bfba1bfd97f3ceb25c7
maironcorrales <maironcorrales@gmail.com>
2015-11-09 12:16:21 -0600

    visualizador de audio

--- app/src/main/java/com/example/geo_2/proyectoso/PlayActivity.java
+++ app/src/main/java/com/example/geo_2/proyectoso/PlayActivity.java
@@ -1,10 +1,20 @@
 package com.example.geo_2.proyectoso;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.audiofx.Visualizer;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,13 +23,28 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import java.io.IOException;
+
 
 public class PlayActivity extends AppCompatActivity {
 
+    private final String MEDIA_PATH = new String("storage/sdcard0/Download/owari-no-seraph-ending-full.mp3");
+    private static final float VISUALIZER_HEIGHT_DIP = 50f;
+
+    //Your MediaPlayer
+    MediaPlayer mp;
+
+    //Vizualization
+    private Visualizer mVisualizer;
+
+    private LinearLayout mLinearLayout;
+    private VisualizerView mVisualizerView;
+    private TextView mStatusTextView;
     private ImageView btnPlay;
     private boolean flag = true;
     private TextView clientName;
     private ActionBar actionBar;
+
     Toolbar toolbar;
     DrawerLayout drawerLayout;
 
@@ -46,6 +71,52 @@ public class PlayActivity extends AppCompatActivity {
         setupNavigationDrawerContent(navigationView);
 
        // receiveName();
+        //mStatusTextView = new TextView(this);
+
+        //Create new LinearLayout ( because main.xml is empty )
+        mLinearLayout = (LinearLayout)findViewById(R.id.linear_layout);
+        //mLinearLayout.addView(mStatusTextView);
+
+        //set content view to new Layout that we create
+        //setContentView(mLinearLayout);
+
+        //start media player - like normal
+        mp = new MediaPlayer();
+        mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        try {
+            mp.setDataSource(MEDIA_PATH); // set data source our URL defined
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {   //tell your player to go to prepare state
+            mp.prepare();
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        //Start your stream / player
+        mp.start();
+
+        //setup your Vizualizer - call method
+        setupVisualizerFxAndUI();
+
+        //enable vizualizer
+        mVisualizer.setEnabled(true);
+
+        //Info text
+        mStatusTextView.setText("Playing audio...");
     }
 
     public void receiveName(){
@@ -68,6 +139,92 @@ public class PlayActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void setupVisualizerFxAndUI() {
+        // Crear visualizador
+        mVisualizerView = new VisualizerView(this);
+        mVisualizerView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                (int) (VISUALIZER_HEIGHT_DIP * getResources().getDisplayMetrics().density)));
+        mLinearLayout.addView(mVisualizerView);
+        // Crear visualizador y pegarlo al media player
+        mVisualizer = new Visualizer(mp.getAudioSessionId());
+        mVisualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
+        mVisualizer.setDataCaptureListener(new Visualizer.OnDataCaptureListener() {
+            public void onWaveFormDataCapture(Visualizer visualizer, byte[] bytes,
+                                              int samplingRate) {
+                mVisualizerView.updateVisualizer(bytes);
+            }
+
+            public void onFftDataCapture(Visualizer visualizer, byte[] bytes, int samplingRate) {
+            }
+        }, Visualizer.getMaxCaptureRate() / 2, true, false);
+    }
+
+    // clase que me permite dibujar las ondas de bytes.
+    class VisualizerView extends View {
+        private byte[] mBytes;
+        private float[] mPoints;
+        private Rect mRect = new Rect();
+
+        private Paint mForePaint = new Paint();
+
+        public VisualizerView(Context context) {
+            super(context);
+            init();
+        }
+
+        private void init() {
+            mBytes = null;
+
+            mForePaint.setStrokeWidth(1f);
+            mForePaint.setAntiAlias(true);
+            mForePaint.setColor(Color.rgb(0, 128, 255));
+        }
+
+        public void updateVisualizer(byte[] bytes) {
+            mBytes = bytes;
+            invalidate();
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            if (mBytes == null) {
+                return;
+            }
+
+            if (mPoints == null || mPoints.length < mBytes.length * 4) {
+                mPoints = new float[mBytes.length * 4];
+            }
+
+            mRect.set(0, 0, getWidth(), getHeight());
+
+            for (int i = 0; i < mBytes.length - 1; i++) {
+                mPoints[i * 4] = mRect.width() * i / (mBytes.length - 1);
+                mPoints[i * 4 + 1] = mRect.height() / 2
+                        + ((byte) (mBytes[i] + 128)) * (mRect.height() / 2) / 128;
+                mPoints[i * 4 + 2] = mRect.width() * (i + 1) / (mBytes.length - 1);
+                mPoints[i * 4 + 3] = mRect.height() / 2
+                        + ((byte) (mBytes[i + 1] + 128)) * (mRect.height() / 2) / 128;
+            }
+
+            canvas.drawLines(mPoints, mForePaint);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (isFinishing() && mp != null) {
+            mVisualizer.release();
+            //mEqualizer.release();
+            mp.release();
+            mp = null;
+        }
     }
 
     //Metodo para el navigation drawer
